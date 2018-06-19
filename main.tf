94ea786c4930d22495c54ea96cf083f86e6a470b
Forrest <forrest.waters@wpengine.com>
2018-06-19 06:36:35 -0500

    adding a load balancer

--- main.tf
+++ main.tf
@@ -37,6 +37,8 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.available.names}"]
 
+  load_balancers    = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
   min_size = 2
   max_size = 10
 
@@ -45,4 +47,7 @@ resource "aws_autoscaling_group" "example" {
     value               = "terraform-asg-example"
     propagate_at_launch = true
   }
+}
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
